70c82654c7a8b40cfb16e992b91b1485948b45c7
Rodrigo Henrique <rhs.buiu@gmail.com>
2025-04-17 09:59:13 -0300

    :tada: Inicialização do Projeto

---
+++ src/main/java/com/grupo5turmajava81/aplicativocarona/ProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.grupo5turmajava81.aplicativocarona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorApplication.class, args);
+	}
+
+}
